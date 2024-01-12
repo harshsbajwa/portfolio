a1aa0ad877588b091e9ff50197fafcba54a3ceb6
harshsbajwa <harsh.bajwa@mail.utoronto.ca>
2024-01-12 06:31:37 -0500

    blog link

--- components/Header/index.js
+++ components/Header/index.js
@@ -76,7 +76,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   {showResume && (
                     <Button
                       onClick={() =>
-                        window.open("mailto:hello@chetanverma.com")
+                        window.open("mailto:harsh.bajwa@mail.utoronto.ca")
                       }
                     >
                       Resume
@@ -84,7 +84,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:hello@chetanverma.com")}
+                    onClick={() => window.open("mailto:harsh.bajwa@mail.utoronto.ca")}
                   >
                     Contact
                   </Button>
@@ -107,7 +107,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:hello@chetanverma.com")}
+                    onClick={() => window.open("mailto:harsh.bajwa@mail.utoronto.ca")}
                   >
                     Contact
                   </Button>
@@ -143,7 +143,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button onClick={() => window.open("mailto:harsh.bajwa@mail.utoronto.ca")}>
               Contact
             </Button>
             {mounted && theme && data.darkMode && (
@@ -172,7 +172,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button onClick={() => window.open("mailto:harsh.bajwa@mail.utoronto.ca")}>
               Contact
             </Button>
 
